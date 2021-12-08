8476ab1aaa4d495f982d7a37928053e2fdc6056b
Yash2612 <yash9b@gmail.com>
2021-12-08 09:11:15 +0530

    UPD

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 let loginForm = document.querySelector('.header .login-form');
 
+document.querySelector("#sign-in").onclick=()=>{
+    let password=document.forms["loginform"]["password"].value;
+    let digit=false,smallchar=false,capitalchar=false,valid=true;
+    if(password.length<8) valid=false;
+    for(let i=0;i<password.length;i++)
+    {
+        if(password[i]>='0' && password[i]<='9') {
+            digit=true;
+        }
+        if(password[i]>='a' && password[i]<='z') {
+            smallchar=true;
+        }
+        if(password[i]>='A' && password[i]<='Z') {
+            capitalchar=true;
+        }
+    }
+    valid=valid&digit;
+    valid=valid&smallchar;
+    valid=valid&capitalchar;
+    if(valid==false){
+        alert("Enter a valid passwrod containg atleast 8 character including small and capital alphabats and numbers");
+        return false;
+    }
+}
+
+
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
     navbar.classList.remove('active');
@@ -22,6 +48,7 @@ window.onscroll = () =>{
         document.querySelector('.header').classList.remove('active');
     }
 }
+
 
 window.onload = () =>{
     if(window.scrollY > 0){
